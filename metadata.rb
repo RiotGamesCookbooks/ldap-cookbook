39ad4d024eb8e88caa2f2f8db06376b459f359c2
Alan Willis <alan@amekoshi.com>
2014-08-19 21:40:38 +0100

    remove long_description

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,4 @@ maintainer       'Alan Willis'
 maintainer_email 'alwillis@riotgames.com'
 license          'Apache v2.0'
 description      'Provides generic lightweight resources for managing LDAP objects'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
